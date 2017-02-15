28f9e9c23a5b3c66903ba288173ed64f6194f237
dviramontes <dviramontes@gmail.com>
2017-02-14 20:49:22 -0500

    refactor: hardware code into own file

---
+++ hardware.js
@@ -0,0 +1,9 @@
+// const LedMatrix = require("./index");
+// const matrix = new LedMatrix(16);
+// matrix.fill(255, 50, 100);
+// matrix.setPixel(0, 0, 0, 50, 255);
+//
+// setTimeout(() => {
+//     matrix.fill(155, 50, 100);
+// matrix.setPixel(0, 0, 0, 50, 255);
+// }, 2000);
