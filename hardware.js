8cf3443233c2a85800da9330094a054b6e3c7d5f
dviramontes <dviramontes@gmail.com>
2017-02-14 20:56:28 -0500

    chore: adds hardware dep

--- hardware.js
+++ hardware.js
@@ -1,9 +1,10 @@
-// const LedMatrix = require("./index");
-// const matrix = new LedMatrix(16);
-// matrix.fill(255, 50, 100);
-// matrix.setPixel(0, 0, 0, 50, 255);
-//
-// setTimeout(() => {
-//     matrix.fill(155, 50, 100);
-// matrix.setPixel(0, 0, 0, 50, 255);
-// }, 2000);
+const LedMatrix = require("node-rpi-rgb-led-matrix");
+const matrix = new LedMatrix(16);
+
+matrix.fill(255, 50, 100);
+matrix.setPixel(0, 0, 0, 50, 255);
+
+setTimeout(() => {
+    matrix.fill(155, 50, 100);
+    matrix.setPixel(0, 0, 0, 50, 255);
+}, 2000);
