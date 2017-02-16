3e650d5d0babe8b0994deec9c030d552d1dfb7a9
dviramontes <dviramontes@gmail.com>
2017-02-16 12:41:46 -0500

    fix: kill existing proc on run

--- src/hardware.js
+++ src/hardware.js
@@ -1,9 +1,12 @@
-const { execFile }  = require('child_process');
+const {execFile}  = require('child_process');
 
 // pi@raspberrypi:~/Documents/rpi-rgb-led-matrix/python/samples $ sudo python newtext.py -r=16 -t="Good Bye Tyler :'("
 
-export function display (name) {
-  execFile('python', [`/home/pi/Documents/rpi-rgb-led-matrix/python/samples/newtext.py`, `-r=16`, `-t="${name}"`], (error, stdout, stderr) => {
+let proc;
+
+export function display(name) {
+    if (proc) proc.kill('SIGINT');
+    proc = execFile('python', [`/home/pi/Documents/rpi-rgb-led-matrix/python/samples/newtext.py`, `-r=16`, `-t="${name}"`], (error, stdout, stderr) => {
         if (error) {
             throw error;
         }
